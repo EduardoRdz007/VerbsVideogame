e53f14f0a02a7081a24a6ed6e9a82573ae5034c5
EduardoRdz007 <edurdz010@gmail.com>
2022-10-14 12:35:12 -0500

    Se agregaron mas verbos

--- js/verbs.js
+++ js/verbs.js
@@ -2,10 +2,30 @@ const verbs = [
     'open',
     'accept',
     'love',
+    'paint',
+    'play',
+    'follow',
+    'travel',
+    'write',
+    'run',
+    'close',
+    'dance',
+    'stop',
+
 ]
 
 const verbos = [
     'abrir',
     'aceptar',
     'amar',
+    'pintar',
+    'jugar',
+    'seguir',
+    'viajar',
+    'escribir',
+    'correr',
+    'cerrar',
+    'bailar',
+    'alto'
+
 ]   
